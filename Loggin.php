735e218a8335870efd770999920d20c8335d9932
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-05 20:08:15 +0530

    Update Loggin.php

--- Loggin.php
+++ Loggin.php
@@ -11,7 +11,14 @@
     <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
     </head><body>
     <script>
-
+    Swal.fire({
+        icon: 'warning',
+        title: 'Missing Fields',
+        text: 'Please enter both email and password.',
+        confirmButtonText: 'OK'
+    }).then(() => {
+        window.location.href = 'Loggin.html';
+    });
     </script>
     </body></html>";
     exit();
