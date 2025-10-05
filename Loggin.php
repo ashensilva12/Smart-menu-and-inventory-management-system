d107e96270153ee2b6bda86210c3ec10e73b0f05
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-05 20:09:23 +0530

    Update Loggin.php

--- Loggin.php
+++ Loggin.php
@@ -24,4 +24,8 @@
     exit();
     }
     $con = new mysqli('localhost:6368', 'root', '1234', 'resturent');
+
+    if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
 ?>
