14f3a17464e296e07a2e971004c54c2854e1edd5
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-05 20:07:58 +0530

    Update Loggin.php

--- Loggin.php
+++ Loggin.php
@@ -6,7 +6,14 @@
 
     // If fields are empty, show SweetAlert2 popup and redirect
     if ($Email === '' || $Password === '') {
+    echo "
+    <html><head>
+    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
+    </head><body>
+    <script>
 
+    </script>
+    </body></html>";
     exit();
     }
 ?>
