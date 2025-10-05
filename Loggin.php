2d8300b67eb403295e808fd310546cd5105f5cba
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-05 20:07:37 +0530

    Update Loggin.php

--- Loggin.php
+++ Loggin.php
@@ -3,4 +3,10 @@
 
     $Email = trim($_POST['email'] ?? '');
     $Password = trim($_POST['password'] ?? '');
+
+    // If fields are empty, show SweetAlert2 popup and redirect
+    if ($Email === '' || $Password === '') {
+
+    exit();
+    }
 ?>
