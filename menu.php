6a715e97fb9f7c8dcae4cf72e2a6f37cbc842fc1
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-11 20:53:13 +0530

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,4 +1,7 @@
 <?php
     header('Content-Type: text/html');
     $con = new mysqli('localhost:6368', 'root', '1234', 'resturent');
+    if ($con->connect_error) {
+    die("<p class='error'>Database connection failed: " . $con->connect_error . "</p>");
+}
 ?>
