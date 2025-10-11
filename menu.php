30398dcb3838ca0cc5074b59fc58f6faf07994c3
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-11 20:54:00 +0530

    Update menu.php

--- menu.php
+++ menu.php
@@ -2,6 +2,9 @@
     header('Content-Type: text/html');
     $con = new mysqli('localhost:6368', 'root', '1234', 'resturent');
     if ($con->connect_error) {
-    die("<p class='error'>Database connection failed: " . $con->connect_error . "</p>");
+        die("<p class='error'>Database connection failed: " . $con->connect_error . "</p>");
+
+    $sql = "SELECT * FROM menu ORDER BY id DESC";
+    $result = $con->query($sql);
 }
 ?>
