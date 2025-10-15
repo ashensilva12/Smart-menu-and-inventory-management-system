573aa07a885d518dc9fe5375d11918acbc482907
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-15 22:08:41 +0530

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -2,4 +2,10 @@
     require 'vendor/autoload.php';
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars(trim($_POST['name']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $subject = htmlspecialchars(trim($_POST['subject']));
+    $message = htmlspecialchars(trim($_POST['message']));
 ?>
