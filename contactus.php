aac378e36b3adf30c718b5fd5e8526da672cf274
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-15 22:09:42 +0530

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -10,4 +10,17 @@
     $message = htmlspecialchars(trim($_POST['message']));
 
     $mail = new PHPMailer(true);
+
+        try {
+        // SMTP configuration
+        $mail->isSMTP();
+        $mail->Host       = 'smtp-relay.brevo.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = '93b60b001@smtp-brevo.com';           // Your Gmail
+        $mail->Password   = 'U0ES13KZ4mALxV5g';             // Gmail App Password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port       = 587;
+
+        
+
 ?>
