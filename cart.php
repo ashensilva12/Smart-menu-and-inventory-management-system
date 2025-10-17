4192b365967ca050510151670dcdfc9cc4dc29b0
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-17 18:09:54 +0530

    Update cart.php

--- cart.php
+++ cart.php
@@ -277,6 +277,14 @@ try {
       </script>
         </body>
         </html>';
+            // -------- Generate PDF (Dompdf) --------
+    require_once __DIR__ . '/vendor/autoload.php';
+    $dompdf = new Dompdf\Dompdf();
+    // Allow remote images if you add a logo later
+    $dompdf->set_option('isRemoteEnabled', true);
+    $dompdf->loadHtml($html);
+    $dompdf->setPaper('A4', 'portrait');
+    $dompdf->render();
     }
 ?>
 
