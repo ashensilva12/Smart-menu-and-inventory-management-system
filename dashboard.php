affb403275c698ed4ca112bd6c2fa35e6aec24fc
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-19 18:46:02 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -15,6 +15,16 @@
         }
         $sql = "SELECT * FROM orders";
         $result = $con->query($sql);
+        if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+        echo "<tr>
+                <td>{$row['orderID']}</td>
+                <td>{$row['customer']}</td>
+                <td>{$row['items']}</td>
+                <td>{$row['total']}</td>
+            </tr>";
+            }
+        } 
 
     }
 ?>
