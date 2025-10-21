e96bde322d9d1b1ae1bf11736e1a543d8b5cc06e
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-21 20:29:33 +0530

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -1,3 +1,8 @@
 <?php
     $con = new mysqli('localhost:6368', 'root', '1234', 'resturent');
+
+    // Check DB connection
+    if ($con->connect_error) {
+        die("Connection failed: " . $con->connect_error);
+}
 ?>
