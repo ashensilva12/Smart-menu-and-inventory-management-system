d510e225035b645024089068bc2d46463afb09d8
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-23 20:25:38 +0530

    Update additem.php

--- additem.php
+++ additem.php
@@ -4,4 +4,10 @@
   $category = $_POST['category'] ?? '';
   $stock = $_POST['stock'] ?? '';
   $unit = $_POST['unit'] ?? '';
+
+$cri1 = $itemID != "";
+$cri2 = $itemname != "";
+$cri3 = $category != "";
+$cri4 = $stock != "";
+$cri5 = $unit != "";
 ?>
