bac37c09dbfff2df7d6dc91ce64a63df1f6b8eef
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-23 20:26:27 +0530

    Update additem.php

--- additem.php
+++ additem.php
@@ -14,4 +14,7 @@ $cri5 = $unit != "";
 if (!$cri1 || !$cri2 || !$cri3 || !$cri4 || !$cri5) exit();
 
 $con = new mysqli('localhost:6368', 'root', '1234', 'resturent');
+
+$data = "INSERT INTO invitems(itemID, itemName, category, currentStock, unit, status) 
+         VALUES('$itemID', '$itemname', '$category', '$stock', '$unit', 'in-stock')";
 ?>
