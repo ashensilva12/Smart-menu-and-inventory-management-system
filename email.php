ea305b99beab309e38944bf6f1a2896c8b8e8a59
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-27 20:47:32 +0530

    Update email.php

--- email.php
+++ email.php
@@ -12,5 +12,6 @@
         $mail->Host = 'smtp-relay.brevo.com'; // Gmail SMTP server
         $mail->SMTPAuth = true;
         $mail->Username = '93b60b001@smtp-brevo.com'; // Your Gmail
+        $mail->Password = 'U0ES13KZ4mALxV5g'; // App password, not your Gmail password
     }
 ?>
