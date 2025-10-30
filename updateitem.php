ec71f9c0475b269cbda86a2e5e45f0210babb286
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-30 22:08:50 +0530

    Update updateitem.php

--- updateitem.php
+++ updateitem.php
@@ -15,4 +15,16 @@
 
   $data = "SELECT currentStock FROM invitems WHERE itemID='$itemID'";
   $result = $con->query($data);
+
+  echo "<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+<body>";
+    
+echo "</body></html>";
+$con->close();
 ?>
