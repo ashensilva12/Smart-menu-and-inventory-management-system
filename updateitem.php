7930fdbdfd9a5334cdacaf1fe540b5297420195c
ashensilva12 <ashenlakshitha12@gmail.com>
2025-10-30 22:12:17 +0530

    Update updateitem.php

--- updateitem.php
+++ updateitem.php
@@ -22,6 +22,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
   </head>
 <body>";
     if ($result && $result->num_rows == 1) {
