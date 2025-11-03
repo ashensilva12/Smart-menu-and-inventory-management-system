b904b787f85f5ec2d8032b44de1517922e98b04d
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-03 12:40:02 +0530

    Update deletemenu.php

--- deletemenu.php
+++ deletemenu.php
@@ -4,5 +4,5 @@ $con = new mysqli('localhost:6368', 'root', '1234', 'resturent');
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
-
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['id'])) {
 ?>
