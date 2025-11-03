21ac5d8d951a790ae349deb508aa1157886f81b8
ashensilva12 <ashenlakshitha12@gmail.com>
2025-11-03 12:39:38 +0530

    Update deletemenu.php

--- deletemenu.php
+++ deletemenu.php
@@ -1,4 +1,8 @@
 <?php
 // deletemenuitem.php
 $con = new mysqli('localhost:6368', 'root', '1234', 'resturent');
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
+
 ?>
