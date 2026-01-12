9ebe3d1fb63723a13a36d98272cb02d057863d94
ashensilva12 <ashenlakshitha12@gmail.com>
2026-01-12 13:20:02 +0530

    Create admin_only.php

---
+++ admin_only.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+if (!isset($_SESSION['admin_username'])) {
+    header('Location: admin_login.html');
+    exit();
+}
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+header('Expires: 0');
